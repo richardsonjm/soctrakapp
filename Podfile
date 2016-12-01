c28d9b5e05d94ac0002fb030a77f882339a3d254
John Richardson <richardsonjm@gmail.com>
2016-12-01 14:29:37 -0500

    Add Podfile

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'soctrakapp' do
+  pod 'SSKeychain', '~> 1.2.2' 
+  pod 'SVProgressHUD', '~> 1.0'
+end
---
+++ Pods/Target Support Files/Pods-soctrakapp/Pods-soctrakapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_soctrakapp : NSObject
+@end
+@implementation PodsDummy_Pods_soctrakapp
+@end
