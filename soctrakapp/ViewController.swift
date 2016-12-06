1bb24b436d88270f3ac7536314457dedf603629d
John Richardson <richardsonjm@gmail.com>
2016-12-06 10:21:10 -0500

    Update api request to use alamofire

--- soctrakapp/ViewController.swift
+++ soctrakapp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -23,48 +24,30 @@ class ViewController: UIViewController {
 
     func getPartnerships() {
         let partnershipsEndpoint: String = "http://localhost:5000/v1/partnerships/2"
-        guard let url = URL(string: partnershipsEndpoint) else {
-            print("Error: cannot create URL")
-            return
-        }
-        let urlRequest = URLRequest(url: url)
-        let session = URLSession.shared
-        let task = session.dataTask(with: urlRequest) {
-            (data, response, error) in
-            guard error == nil else {
-                print("error calling GET on \(partnershipsEndpoint)")
-                print(error!)
-                return
-            }
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            do {
-                guard let partnership = try JSONSerialization.jsonObject(with: responseData, options: [])
-                    as? [String: Any] else {
-                        print("error trying to convert data to JSON")
-                        return
+        Alamofire.request(partnershipsEndpoint)
+            .responseJSON { response in
+                // check for errors
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling GET on \(partnershipsEndpoint)")
+                    print(response.result.error!)
+                    return
                 }
-                // now we have the partnerships
-                // let's just print it to prove we can access it
-                print("The partnership is: " + partnership.description)
                 
-                // the partnerships object is a dictionary
-                // so we just access the user_id using the "user_id" key
-                // so check for a title and print it if we have one
-                guard let partnershipUserId = partnership["user_id"] as? Int else {
-                    print("Could not get partnership user_id from JSON")
+                // make sure we got some JSON since that's what we expect
+                guard let json = response.result.value as? [String: Any] else {
+                    print("didn't get partnership object as JSON from API")
+                    print("Error: \(response.result.error)")
+                    return
+                }
+                
+                // get and print the title
+                guard let partnershipUserId = json["user_id"] as? Int else {
+                    print("Could not get todo title from JSON")
                     return
                 }
                 print("The user_id is: \(partnershipUserId)")
-            } catch  {
-                print("error trying to convert data to JSON")
-                return
-            }
         }
-        
-        task.resume()
     }
 }
 
